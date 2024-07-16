759233391714cd351339b6dda03cf19567298967
codal-mgajjar <mgajjar@codal.com>
2024-07-16 12:42:31 +0530

    package.json added

--- lambda/index.js
+++ lambda/index.js
@@ -1,8 +1,8 @@
-export const handler = async (event: any = {}): Promise<any> => {
+exports.handler = async (event) => {
     console.log("request:", JSON.stringify(event, undefined, 2));
     return {
         statusCode: 200,
         headers: { "Content-Type": "text/plain" },
-        body: `Hello, CDK!\n`
+        body: `Hello, CDK! You've hit ${event.path}\n`
     };
 };
