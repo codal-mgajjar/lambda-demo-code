19478e7538e5ea125f268b33f9ddbaa67a950990
codal-mgajjar <mgajjar@codal.com>
2024-07-16 14:16:59 +0530

    index.js

--- lambda/hello-world.ts
+++
@@ -1,8 +0,0 @@
-export const handler = async (event: any = {}): Promise<any> => {
-    console.log("request:", JSON.stringify(event, undefined, 2));
-    return {
-        statusCode: 200,
-        headers: { "Content-Type": "text/plain" },
-        body: `Hello, CDK!\n`
-    };
-};
