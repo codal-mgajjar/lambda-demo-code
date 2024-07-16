a5cf6931382352cac61a9aaba85b367ca61eee65
codal-mgajjar <mgajjar@codal.com>
2024-07-16 13:07:32 +0530

    package.json added

--- lambda/hello-world.ts
+++ lambda/hello-world.ts
@@ -1,8 +1,8 @@
-exports.handler = async (event) => {
+export const handler = async (event: any = {}): Promise<any> => {
     console.log("request:", JSON.stringify(event, undefined, 2));
     return {
         statusCode: 200,
         headers: { "Content-Type": "text/plain" },
-        body: `Hello, CDK! You've hit ${event.path}\n`
+        body: `Hello, CDK!\n`
     };
 };
